082f1074c7eb81a4c85c2bfdc17ac814947e1ee3
Melissa Jacobi <miffyjacobi@gmail.com>
2024-01-08 15:30:27 -0800

    exercise-2

---
+++ components/Buttons/index.js
@@ -0,0 +1,12 @@
+import styles from './Button.module.css'
+
+export default function Button({
+    title="default",
+    color
+}) {
+    return(
+        <> 
+            <button style={{backgroundColor: color}}>{title}</button>
+        </>
+    )
+}
---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Header from "@/components/Header"
+import styles from '@/styles/About.module.css'
+import Counter from "@/components/Counter"
+
+export default function About() {
+    return(
+        <> 
+            <h1>About Page</h1>
+            <Header/>
+            <Counter/>
+        </>
+    )
+}
